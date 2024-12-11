ad86be7a08686ecbaf6acc0213817f357fb50132
Gabriellmrs <gabrielmoriss755@gmail.com>
2024-12-11 13:23:58 -0300

    Carrega dados de usuario na tela

--- FRONT/cadastro/src/pages/Consultar/index.jsx
+++ FRONT/cadastro/src/pages/Consultar/index.jsx
@@ -1,13 +1,42 @@
-import './style.css'
+import { useEffect, useState } from 'react';
+import './style.css';
+import api from '../../services/api';
 
 function Consultar() {
+    const [usuarios, setUsuarios] = useState([]);
 
+    async function getUsuarios() {
+        try {
+            const response = await api.get('/usuarios');
+            setUsuarios(response.data); // Atualiza a lista de usuários
+        } catch (error) {
+            console.error('Erro ao buscar usuários:', error);
+        }
+    }
+
+    useEffect(() => {
+        getUsuarios(); // Chama a função ao carregar o componente
+    }, []);
 
     return (
         <div className='container'>
             <h1>CONSULTAR</h1>
+            {usuarios.length > 0 ? (
+                usuarios.map((usuario) => (
+                    <div key={usuario.ID} className='card'>
+                        <div>
+                            <p>ID: <span>{usuario.ID}</span></p>
+                            <p>Nome: <span>{usuario.NOME}</span></p>
+                            <p>Idade: <span>{usuario.IDADE}</span></p>
+                            <p>Email: <span>{usuario.EMAIL}</span></p>
+                        </div>
+                    </div>
+                ))
+            ) : (
+                <p>Nenhum usuário encontrado.</p>
+            )}
         </div>
-    )
+    );
 }
 
-export default Consultar
+export default Consultar;
--- FRONT/cadastro/src/pages/Home/index.jsx
+++ FRONT/cadastro/src/pages/Home/index.jsx
@@ -1,4 +1,5 @@
 import './style.css'
+import api from '../../services/api'
 
 function Home() {
   
