fcec8d709096149bc9fcc8ac7ae5a396d0eb23b5
Gabriellmrs <gabrielmoriss755@gmail.com>
2024-12-13 13:35:14 -0300

    Adicionando o metodo POST

--- BACK/router/routes.js
+++ BACK/router/routes.js
@@ -1,5 +1,5 @@
 import { Router } from "express"
-import {connectToDatabase} from "../db/connection.js"
+import { connectToDatabase } from "../db/connection.js"
 
 const router = Router();
 
@@ -9,8 +9,27 @@ router.get("/usuarios", async (req, res) => {
         const resultado = await conexao.request().query("SELECT * FROM tb_usuario")
         res.json(resultado.recordset)
     }
-    catch(err) {
+    catch (err) {
         res.status(500).send('Erro ao buscar clientes.');
+    }
+})
+
+router.post("/cadastrar", async (req, res) => {
+    const { NOME, IDADE, EMAIL } = req.body
+    try {
+        const conexao = await connectToDatabase();
+        await conexao
+            .request()
+            .input('NOME', NOME)
+            .input('IDADE', IDADE)
+            .input('EMAIL', EMAIL)
+            .query(
+                `INSERT INTO tb_usuario (NOME, IDADE, EMAIL)
+         VALUES (@NOME, @IDADE, @EMAIL)`
+            )
+            res.status(201).send('Usuario criado com sucesso')
+    } catch (err) {
+        res.status(500).send('Erro ao criar usuario.');
     }
 })
 
